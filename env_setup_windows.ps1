1d9410c0912782c60bdc2b4c9b62e1133b7e445f
Yehor Solonukha <yehor@student.kul.edu.pl>
2025-12-28 13:54:37 +0100

    temporary solution for env set up

---
+++ env_setup_windows.ps1
@@ -0,0 +1,11 @@
+# Build images in the correct directory
+docker compose build
+
+# Store current directory (absolute path)
+$cwd = (Get-Location).Path
+
+# Open tabs in the SAME directory
+wt.exe `
+    new-tab --title "Server"   --startingDirectory "$cwd" powershell -NoExit -Command "docker compose run --rm server" `
+    ';' new-tab --title "Client 1" --startingDirectory "$cwd" powershell -NoExit -Command "docker compose run --rm -it client1" `
+    ';' new-tab --title "Client 2" --startingDirectory "$cwd" powershell -NoExit -Command "docker compose run --rm -it client2"

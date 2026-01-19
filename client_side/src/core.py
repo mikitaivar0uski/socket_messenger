c7eba89f52558c3f0425be1ed7e5b1803b2dce57
Yehor Solonukha <yehor@student.kul.edu.pl>
2026-01-19 22:36:33 +0100

    Refactor client_side

---
+++ client_side/src/core.py
@@ -0,0 +1,20 @@
+class Core():
+    def __init__(self, network, ui):
+        self.network = network
+        self.ui = ui
+        self.running = True
+
+    def on_user_input(self, message: str):
+        if message:
+            self.network.send_to_server(message)
+
+    def on_server_message(self, message: str):
+        if not message:
+            self.ui.display("Server disconnected.")
+            self.stop()
+        else:
+            self.ui.display(message)
+
+    def stop(self):
+        self.running = False
+        self.network.close_server_connection()
--- env_setup_windows2.ps1
+++ env_setup_windows2.ps1
@@ -3,5 +3,5 @@ $cwd = (Get-Location).Path
 
 wt.exe `
     new-tab --title "Server"   --startingDirectory "$cwd" powershell -NoExit -Command "py server_side/src/server_main.py" `
-    ';' new-tab --title "Client 1" --startingDirectory "$cwd" powershell -NoExit -Command "py client_side/src/client_main.py" `
-    ';' new-tab --title "Client 2" --startingDirectory "$cwd" powershell -NoExit -Command "py client_side/src/client_main.py"
+    ';' new-tab --title "Client 1" --startingDirectory "$cwd" powershell -NoExit -Command "py client_side/src/main.py" `
+    ';' new-tab --title "Client 2" --startingDirectory "$cwd" powershell -NoExit -Command "py client_side/src/main.py"

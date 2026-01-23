a2a0cd22f6a0d3b1ddaa7b9d71e1f92f8a999d7c
Yehor Solonukha <yehor@student.kul.edu.pl>
2026-01-23 12:46:18 +0100

    twicked dockerfiles and env setups a little bit

--- env_setup_windows2.ps1
+++ env_setup_windows2.ps1
@@ -1,6 +1,7 @@
-# Save as run_messenger.ps1
+# store current directory (absolute path)
 $cwd = (Get-Location).Path
 
+# open tabs in the same dir
 wt.exe `
     new-tab --title "Server"   --startingDirectory "$cwd" powershell -NoExit -Command "py server_side/src/server_main.py" `
     ';' new-tab --title "Client 1" --startingDirectory "$cwd" powershell -NoExit -Command "py client_side/src/main.py" `

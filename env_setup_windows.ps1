93a190a8bcb8ce95a9273bdf1b9e0a6c371b254c
Yehor Solonukha <yehor@student.kul.edu.pl>
2026-01-28 22:31:12 +0100

    fixed issues with containers, working properly

--- env_setup_windows.ps1
+++
@@ -1,9 +0,0 @@
-docker compose build
-
-# store current directory (absolute path)
-$cwd = (Get-Location).Path
-
-# open tabs in the same dir
-wt.exe `
-     new-tab --title "Client 1" --startingDirectory "$cwd" powershell -NoExit -Command "docker compose run --rm client1" `
-    ';' new-tab --title "Client 2" --startingDirectory "$cwd" powershell -NoExit -Command "docker compose run --rm client2"
